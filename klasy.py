01ab54b04c8afa612583f2deffbe9e8f173389fd
anialaziuk <anna_laziuk@wp.pl>
2026-01-03 10:26:35 +0000

    Added imie to Czlowiek.

--- klasy.py
+++ klasy.py
@@ -3,12 +3,17 @@
 class Czlowiek:
     # istota
     gatunek = "Homo sapiens"
-    def __init__(self):
+    def __init__(self, imie):
         # istnienie
         # specyficzna metoda bo to jest konstruktor
-        print("Niech powstanie czlowiek")
+        print(f"Niech powstanie czlowiek o imieniu {imie}")
+        self.imie = imie
 
 #powstanie obiektu
 #gotowanie z przepisu
-adam = Czlowiek() # a = 4 # a = int(4)
+adam = Czlowiek("Adam") # a = 4 # a = int(4)
+ewa = Czlowiek("Ewa")
 print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
